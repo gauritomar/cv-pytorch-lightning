ef061f7ca1e0292533cde49c3f6c77be52a188bf
gauri3 <gauri.t@orangewood.co>
2024-02-09 16:58:29 +0530

    Fixed accuracy function

--- pytorch-tutorials/3-nn-ex.py
+++ pytorch-tutorials/3-nn-ex.py
@@ -112,9 +112,8 @@ def check_accuracy(loader, model):
 
         print(f"Got {num_correct}/{num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}%")
 
-    accuracy = f"{float(num_correct)/float(num_samples):.2f}"
     model.train()
-    return acc
+  
 
 check_accuracy(train_loader, model)
 check_accuracy(test_loader, model)
