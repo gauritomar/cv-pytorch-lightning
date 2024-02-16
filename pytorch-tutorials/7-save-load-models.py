8bf352ff5668ea879d48b21d6080bcb0a044c7dd
gauri3 <gauri.t@orangewood.co>
2024-02-16 10:51:46 +0530

    7/54 complete

---
+++ pytorch-tutorials/7-save-load-models.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F 
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+from tqdm import tqdm
+import torchvision.transforms as transforms
+
+class CNN(nn.Module):
+    def __init__(self, in_channel, num_classes):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3,3), stride=(1, 1), padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+
+'''
+.tar extension stands for tape archive, and indicates that
+the saved file is a compressed archive that may contain multiple
+files or directories
+'''
+def save_checkpoint(state, epoch):
+    filename=f"checkpoints/checkpoint_{epoch+1}.pth.tar"
+    print("=> Saving Checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint):
+    print("=> Loading Checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint['optimiser'])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device: ", device)
+
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 10
+load_model = True
+
+train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=False)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=False)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+model = CNN(in_channel=1, num_classes=10).to(device=device)
+x = torch.rand(64, 1, 28, 28).to(device=device)
+print(model(x).shape)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr= learning_rate)
+
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+best_loss = 10
+for epoch in range(num_epochs):
+    losses = []
+
+    for idx, (data, targets) in tqdm(enumerate(train_loader), total=len(train_loader), leave=False):
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
+        
+        if loss < best_loss:
+            best_loss = loss
+            checkpoint = {'state_dict': model.state_dict(), 'optimiser': optimizer.state_dict()}
+            save_checkpoint(checkpoint, epoch)
+
+    print(f"Epoch: {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+
+
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        if loader.dataset.train:
+            print("Checking accuracy on training data")
+
+        else:
+            print("Checking accuracy on test data")
+
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+
+            scores = model(x)
+
+            _, predictions = scores.max(dim=1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f"Got {num_correct}/{num_samples} with accuracy {float(num_correct)/{num_sampels}*100:.2f}%")
+
+    model.train()
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader, model)
--- pytorch-tutorials/README.md
+++ pytorch-tutorials/README.md
@@ -8,5 +8,5 @@
 | [X] | [Pytorch CNN](https://youtu.be/wnK3uWv_WkU?si=DffDlL9zeDwg29DP) | [4-cnn-ex.py](4-cnn-ex.py)|
 | [X] | [Pytorch RNN](https://youtu.be/Gl2WXLIMvKA?si=Zag66dEnq2bUvgLZ) | [5-rnn-ex.py](5-rnn-ex.py), [5-rnn-gru.py](5-rnn-gru.py), [5-rnn-lstm.py](5-rnn-lstm.py) |
 | [X] | [Pytorch Bi-LSTM](https://youtu.be/jGst43P-TJA?si=itQ7uID3KiWdpzkZ) | [6-bidirectional-lstm.py](6-bidirectional-lstm.py) |
-| [ ] | [Save and Load Models](https://youtu.be/g6kQl_EFn84?si=18L3VvCx4V1_xyOl) | [7-save-load-models.py](7-save-load-models.py) |
+| [X] | [Save and Load Models](https://youtu.be/g6kQl_EFn84?si=18L3VvCx4V1_xyOl) | [7-save-load-models.py](7-save-load-models.py) |
 | [ ] | [Transfer Learning and Finetuning](https://youtu.be/qaDe0qQZ5AQ?si=l9Ps3Yr8hk6R_DeV) | |
